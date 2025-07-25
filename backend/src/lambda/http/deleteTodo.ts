b9ba13aee0689bc1150e8ea89761bc0b58559c00
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-07-25 03:27:27 +0200

    (backend) Bug Fix, Refinement and Formatting

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -1,31 +1,29 @@
-import 'source-map-support/register'
+import "source-map-support/register";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors, httpErrorHandler } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import * as middy from "middy";
+import { cors, httpErrorHandler } from "middy/middlewares";
 
-import { deleteTodo } from '../../businessLogic/ToDo'
-import { getUserId } from '../utils'
-import { createLogger } from '../../utils/logger'
+import { deleteTodo } from "../../businessLogic/ToDo";
+import { getUserId } from "../utils";
+import { createLogger } from "../../utils/logger";
 
-const logger = createLogger('deleteTodo');
+const logger = createLogger("deleteTodo");
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     console.log("Deleting todo");
-    const todoId = event.pathParameters.todoId
+    const todoId = event.pathParameters.todoId;
     // TODO: Remove a TODO item by id
-    const token = event.headers.Authorization.split(' ');
+    const token = event.headers.Authorization.split(" ");
     const deleteData = await deleteTodo(event.pathParameters.todoId, token[1]);
-    logger.info('User deleted a todo item with id: ', {todoId})
-    return deleteData
-  }
-)
+    logger.info("User deleted a todo item with id: ", { todoId });
+    return deleteData;
+  },
+);
 
-handler
-    .use(httpErrorHandler())
-    .use(
-        cors({
-            credentials: true
-        })
-    )
+handler.use(httpErrorHandler()).use(
+  cors({
+    credentials: true,
+  }),
+);
